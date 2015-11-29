d5a76754ccf7204a4da247bf1942659eddbedc60
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-11-29 03:49:44 +0200

    Travis Ubuntu12.04 has outdated TexLive -> install_texlive.sh

---
+++ install_texlive.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+# ~stupid~ awesome TravisUbuntu 12.04 has outdated texlive so we do an automatical install
+
+# idea based on
+# https://github.com/thomasjo/travis-texlive/blob/master/prepare.sh
+# and
+# https://github.com/datakurre/tutkielma/blob/master/texlive.sh
+
+TARGET_DIR=$PWD/texlive
+rm -rf $TARGET_DIR
+
+PROFILE=texlive.profile
+
+cat > $PROFILE <<EOF
+selected_scheme scheme-custom
+TEXDIR $TARGET_DIR
+TEXMFCONFIG $TEXMFSYSCONFIG
+TEXMFHOME $TEXMFLOCAL
+TEXMFLOCAL $TARGET_DIR/texmf-local
+TEXMFSYSCONFIG $TARGET_DIR/texmf-config
+TEXMFSYSVAR $TARGET_DIR/texmf-var
+TEXMFVAR $TEXMFSYSVAR
+binary_x86_64-linux 1
+collection-basic 1
+collection-fontsextra 1
+collection-fontsrecommended 1
+collection-fontutils 1
+collection-formatsextra 1
+collection-genericrecommended 1
+collection-genericextra 1
+collection-humanities 1
+collection-langenglish 1
+collection-langfinnish 1
+collection-langswedish 1
+collection-latex 1
+collection-latexextra 1
+collection-latexrecommended 1
+collection-luatex 1
+collection-mathextra 1
+collection-metapost 1
+collection-pictures 1
+collection-plainextra 1
+collection-pstricks 1
+collection-publishers 1
+collection-science 1
+collection-texworks 1
+collection-xetex 1
+in_place 0
+option_adjustrepo 1
+option_autobackup 1
+option_backupdir tlpkg/backups
+option_desktop_integration 1
+option_doc 0
+option_file_assocs 1
+option_fmt 1
+option_letter 0
+option_menu_integration 1
+option_path 0
+option_post_code 1
+option_src 0
+option_sys_bin /usr/local/bin
+option_sys_info /usr/local/share/info
+option_sys_man /usr/local/share/man
+option_w32_multi_user 0
+option_write18_restricted 1
+portable 1
+EOF
+
+
+rm -rf install*
+
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar xzvf install-tl-unx.tar.gz
+
+install-tl-*/install-tl -profile $PROFILE
+
+export TEXDIR=$TARGET_DIR
+export TEXMFLOCAL=$TARGET_DIR/texmf-local
+export TEXMFSYSCONFIG=$TARGET_DIR/texmf-config
+export TEXMFSYSVAR=$TARGET_DIR/texmf-var
+
+export PATH=$TARGET_DIR/bin/x86_64-linux:$PATH
